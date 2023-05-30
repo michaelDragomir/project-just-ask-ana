96c674b45eeb7c735e56538ece65cec8dc8cdb11
Michael Dragomir <michaeldragomir@Michaels-Laptop.lan>
2023-05-30 13:16:22 -0700

    fetching all users

--- app/random-user/page.tsx
+++ app/random-user/page.tsx
@@ -1,5 +1,41 @@
-const RandomUserHomePage = () => {
-	return <div>random user HP</div>;
+// 'use client';
+
+// import { useEffect, useState } from 'react';
+
+async function fetchUsers() {
+	const response = await fetch('https://randomuser.me/api/');
+	const data = await response.json();
+	return data;
+}
+
+const RandomUserHomePage = async () => {
+	const getUsers = await fetchUsers();
+	const { results } = getUsers;
+	// const [users, setUsers] = useState<any>([]);
+
+	// useEffect(() => {
+	// 	const fetchUsers = async () => {
+	// 		const response = await fetch('https://randomuser.me/api/');
+
+	// 		if (!response.ok) {
+	// 			throw new Error('nope!');
+	// 		}
+
+	// 		const data = response.json();
+
+	// 		setUsers(data);
+	// 	};
+
+	// 	fetchUsers();
+	// }, []);
+
+	console.log('!!!!', results[0].gender);
+
+	return (
+		<>
+			<div>hello</div>
+		</>
+	);
 };
 
 export default RandomUserHomePage;
