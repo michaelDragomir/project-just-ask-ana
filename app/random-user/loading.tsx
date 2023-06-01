e052cd49f2429735a7d4babe99a8200208b2cf92
Michael Dragomir <michaeldragomir@Michaels-Laptop.lan>
2023-06-01 16:07:21 -0700

    added button that refetches data

---
+++ app/random-user/loading.tsx
@@ -0,0 +1,6 @@
+const LoadingPage = () => {
+	console.log('LOADING!!!');
+	return <div>LOADING!!!!!!!!!!!</div>;
+};
+
+export default LoadingPage;
--- components/random-user/MainPage.tsx
+++ components/random-user/MainPage.tsx
@@ -5,8 +5,6 @@ import { useState, useEffect, useCallback } from 'react';
 const MainPage = () => {
 	type Location = any;
 
-	//TO DO:
-	// add a refetch button - it refetches the data
 	// add a search button that filters for that specific search value
 	// make it clickable to select a row - takes you to user specific data.
 	// add a modal - saying..click on header to sort..click on a row to take you to specific user
@@ -51,23 +49,33 @@ const MainPage = () => {
 		[getObjectKeys],
 	);
 
-	useEffect(() => {
-		const fetchPeople = async () => {
-			const response = await fetch('https://randomuser.me/api/?results=15');
-			const data = await response.json();
-			const { results } = data;
-			setUsers(results);
+	const fetchPeople = async () => {
+		const response = await fetch('https://randomuser.me/api/?results=15');
+		if (!response.ok) {
+			throw new Error('no fetchy');
+		}
+		const data = await response.json();
+		const { results } = data;
+		setUsers(results);
+		const getFlattenedLocations = flattenLocationObject(
+			results.map(({ location }: Location) => location),
+		);
+		setFlattenedLocations(getFlattenedLocations);
+	};
 
-			const getFlattenedLocations = flattenLocationObject(
-				results.map(({ location }: Location) => location),
-			);
-			setFlattenedLocations(getFlattenedLocations);
-		};
+	useEffect(() => {
 		fetchPeople();
-	}, [flattenLocationObject]);
+	}, []);
+
+	const refectchDataHandler = () => {
+		fetchPeople();
+	};
 
 	return (
 		<>
+			<button type='button' onClick={refectchDataHandler}>
+				Refectch Data!
+			</button>
 			<table className='mx-auto table-fixed border-collapse border-spacing-2 border-slate-500'>
 				<thead>
 					<tr className='bg-slate-400'>
