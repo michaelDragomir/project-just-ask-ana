10dc4ba3c69b264019072e032b9b9249a450559a
Michael Dragomir <michaeldragomir@Michaels-Laptop.lan>
2023-06-08 14:29:06 -0700

    setting up the styling

--- app/the-cat/page.tsx
+++ app/the-cat/page.tsx
@@ -1,13 +1,16 @@
 import MainPage from '@/components/catAPI/MainPage';
 
 const fetchCatsData = async () => {
-	const response = await fetch('https://api.thecatapi.com/v1/images/search', {
-		headers: {
-			'x-api-key':
-				'live_d7Zz8FWOCt9MQqOtmoBNTMS1R31Q2Yn6RAVcjf2z2guhPU2pQ14Z0jcfum5Evw7I',
+	const response = await fetch(
+		'https://api.thecatapi.com/v1/images/search?limit=10',
+		{
+			headers: {
+				'x-api-key':
+					'live_d7Zz8FWOCt9MQqOtmoBNTMS1R31Q2Yn6RAVcjf2z2guhPU2pQ14Z0jcfum5Evw7I',
+			},
+			cache: 'no-store',
 		},
-		cache: 'no-store',
-	});
+	);
 
 	if (!response.ok) {
 		throw new Error('Failed to fetch data');
--- components/catAPI/MainPage.tsx
+++ components/catAPI/MainPage.tsx
@@ -6,21 +6,25 @@ const MainPage = ({ data }: any) => {
 	console.log('DATA CLIENT index', data[0]);
 	console.log('DATA CLIENT SIDE', data);
 	return (
-		<div>
-			<ul>
-				{data.map((item: any) => {
-					const { id, url, width, height } = item;
-					return (
-						<>
-							<h1>{id}</h1>
-							<Image src={url} width={500} height={333} alt='cat' />
-							<h1>{width}</h1>
-							<h1>{height}</h1>
-						</>
-					);
-				})}
-			</ul>
-		</div>
+		<>
+			<div className='mx-auto w-3/4 max-h-full pt-4 text-xl text-center border-orange-400 border-2'>
+				<div className='border-fuchsia-700 border-2 w-3/4'>
+					<ul>
+						{data.map((item: any) => {
+							const { id, url, width, height } = item;
+							return (
+								<div className='flex border-black border-2'>
+									<Image src={url} width={50} height={33} alt='cat' />
+									{id}
+									<h1>{width}</h1>
+									<h1>{height}</h1>
+								</div>
+							);
+						})}
+					</ul>
+				</div>
+			</div>
+		</>
 	);
 };
 
