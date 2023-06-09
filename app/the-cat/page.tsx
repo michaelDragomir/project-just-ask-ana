69b13e167b644b49f7e7aed8636a2421ec653a52
Michael Dragomir <michaeldragomir@Michaels-Laptop.lan>
2023-06-09 12:36:20 -0700

    rendering cat breed options in a drop down

--- app/the-cat/page.tsx
+++ app/the-cat/page.tsx
@@ -1,37 +1,13 @@
 import MainPage from '@/components/catAPI/MainPage';
 
-const fetchCatsData = async () => {
-	const response = await fetch(
-		'https://api.thecatapi.com/v1/images/search?limit=10',
-		{
-			headers: {
-				'x-api-key':
-					'live_d7Zz8FWOCt9MQqOtmoBNTMS1R31Q2Yn6RAVcjf2z2guhPU2pQ14Z0jcfum5Evw7I',
-			},
-			cache: 'no-store',
-		},
-	);
-
-	if (!response.ok) {
-		throw new Error('Failed to fetch data');
-	}
-
-	const data = await response.json();
-
-	return data;
-};
-
-const RandomUserHomePage = async () => {
-	const catData = await fetchCatsData();
-
-	console.log('!!!', catData);
+const CatHomePage = () => {
 	return (
 		<>
 			<div className='text-center'>
-				<MainPage data={catData} />
+				<MainPage />
 			</div>
 		</>
 	);
 };
 
-export default RandomUserHomePage;
+export default CatHomePage;
--- components/catAPI/MainPage.tsx
+++ components/catAPI/MainPage.tsx
@@ -1,28 +1,47 @@
 'use client';
 
+import { useState, useEffect } from 'react';
 import Image from 'next/image';
 
-const MainPage = ({ data }: any) => {
-	console.log('DATA CLIENT index', data[0]);
-	console.log('DATA CLIENT SIDE', data);
+const MainPage = () => {
+	const [breeds, setBreeds] = useState<any>([]);
+	const [breedImages, setBreedImages] = useState<any>([]);
+	const [selectedbreed, setselectedbreed] = useState<any>(0);
+
+	console.log('--breeds---', breeds);
+
+	useEffect(() => {
+		const getAllBreeds = async () => {
+			const response = await fetch('https://api.thecatapi.com/v1/breeds', {
+				headers: {
+					'x-api-key':
+						'live_d7Zz8FWOCt9MQqOtmoBNTMS1R31Q2Yn6RAVcjf2z2guhPU2pQ14Z0jcfum5Evw7I',
+				},
+				cache: 'no-store',
+			});
+
+			if (!response.ok) {
+				throw new Error('Failed to fetch data');
+			}
+
+			const data = await response.json();
+			setBreeds(data);
+
+			return data;
+		};
+
+		getAllBreeds();
+	}, []);
+
 	return (
 		<>
 			<div className='mx-auto w-3/4 max-h-full pt-4 text-xl text-center border-orange-400 border-2'>
-				<div className='border-fuchsia-700 border-2 w-3/4'>
-					<ul>
-						{data.map((item: any) => {
-							const { id, url, width, height } = item;
-							return (
-								<div className='flex border-black border-2'>
-									<Image src={url} width={50} height={33} alt='cat' />
-									{id}
-									<h1>{width}</h1>
-									<h1>{height}</h1>
-								</div>
-							);
-						})}
-					</ul>
-				</div>
+				<label>Breeds:</label>
+				<select value='cat'>
+					{breeds.map((item: any) => (
+						<option value={item}>{item.name}</option>
+					))}
+				</select>
 			</div>
 		</>
 	);
