4b05768ee56b0971c169627dce035fff8aecbccb
Michael Dragomir <michaeldragomir@Michaels-Laptop.lan>
2023-06-12 11:20:20 -0700

    fetching cat breeds and rendering specifc breeds from drop down menu

--- components/catAPI/ImageCarousel.tsx
+++ components/catAPI/ImageCarousel.tsx
@@ -1,9 +1,10 @@
 import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
+import Image from 'next/image';
 
-const Test = () => {
+const Test = ({ images }) => {
 	return (
-		<div className='w-2/4 mx-auto'>
+		<div className='w-1/3 mx-auto'>
 			<Carousel
 				showArrows={true}
 				showThumbs={false}
@@ -11,15 +12,15 @@ const Test = () => {
 				showStatus={false}
 				className='border-4 border-white rounded-lg'
 			>
-				<div>
-					<img src='https://cdn2.thecatapi.com/images/265.jpg' />
-				</div>
-				<div>
-					<img src='https://cdn2.thecatapi.com/images/4o9.jpg' />
-				</div>
-				<div>
-					<img src='https://cdn2.thecatapi.com/images/9qm.png' />
-				</div>
+				{images.map((image) => (
+					<Image
+						key={image.id}
+						alt={image.id}
+						src={image.url}
+						width={450}
+						height={200}
+					/>
+				))}
 			</Carousel>
 		</div>
 	);
