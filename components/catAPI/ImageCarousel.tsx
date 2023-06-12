c4bd3bdfa35024ca683a493e749fc2270f9effa7
Michael Dragomir <michaeldragomir@Michaels-Laptop.lan>
2023-06-12 11:42:30 -0700

    fixed error

--- components/catAPI/ImageCarousel.tsx
+++ components/catAPI/ImageCarousel.tsx
@@ -2,7 +2,7 @@ import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a lo
 import { Carousel } from 'react-responsive-carousel';
 import Image from 'next/image';
 
-const Test = ({ images }) => {
+const ImageCarousel = ({ images }: any) => {
 	return (
 		<div className='w-1/3 mx-auto'>
 			<Carousel
@@ -12,7 +12,7 @@ const Test = ({ images }) => {
 				showStatus={false}
 				className='border-4 border-white rounded-lg'
 			>
-				{images.map((image) => (
+				{images.map((image: any) => (
 					<Image
 						key={image.id}
 						alt={image.id}
@@ -26,4 +26,4 @@ const Test = ({ images }) => {
 	);
 };
 
-export default Test;
+export default ImageCarousel;
