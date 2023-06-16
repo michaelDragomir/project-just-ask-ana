86b657c9c357460bce9ad84a98e8d88a6cd59f08
Michael Dragomir <michaeldragomir@Michaels-Laptop.lan>
2023-06-16 14:24:35 -0700

    switched to client side data fetching

--- app/the-news/page.tsx
+++ app/the-news/page.tsx
@@ -1,6 +1,33 @@
-import MainPage from '@/components/NewsAPI/MainPage';
-const theNewsHomePage = () => {
-	return <MainPage />;
+const getLatestNews = async () => {
+	const response = await fetch(
+		'https://newsapi.org/v2/top-headlines?country=us',
+		{
+			headers: {
+				'x-api-key': '373a44e44cde4b79bca78c553bcead34',
+			},
+			cache: 'no-store',
+			next: { revalidate: 7200 },
+		},
+	);
+
+	const data = await response.json();
+
+	return data;
 };
 
-export default theNewsHomePage;
+const MainPage = async () => {
+	const newsData = await getLatestNews();
+
+	const { articles } = newsData;
+
+	console.log('NEWS DATA------', articles);
+	return (
+		<div>
+			{articles.map((item: any) => (
+				<li>{item.title}</li>
+			))}
+		</div>
+	);
+};
+
+export default MainPage;
