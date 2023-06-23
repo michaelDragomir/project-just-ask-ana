73fd86f34bcda1d7bbc7efeb5819a7f0e6ddf259
Michael Dragomir <michaeldragomir@Michaels-Laptop.lan>
2023-06-23 12:42:17 -0700

    updated styling

--- components/NewsAPI/TabSlider.tsx
+++ components/NewsAPI/TabSlider.tsx
@@ -18,25 +18,20 @@ const TabSlider = ({ tabItems }: { tabItems: any }) => {
 					className='max-w-screen-xl mx-auto flex items-center gap-x-6 overflow-x-auto text-sm'
 				>
 					{tabItems.map((item: any, idx: any) => (
-						<li
-							key={idx}
-							className={`py-2 border-b ${
+						<button
+							role='tab'
+							aria-selected={activeTab == idx ? true : false}
+							aria-controls={`tabpanel-${idx + 1}`}
+							className={`flex items-center gap-x-2 py-2 border-b px-2 rounded-lg duration-150 hover:text-indigo-600 hover:bg-gray-50 active:bg-gray-100 font-medium ${
 								activeTab == idx
 									? 'border-indigo-600 text-indigo-600'
 									: 'border-white text-gray-500'
 							}`}
+							onClick={() => setActiveTab(idx)}
 						>
-							<button
-								role='tab'
-								aria-selected={activeTab == idx ? true : false}
-								aria-controls={`tabpanel-${idx + 1}`}
-								className='flex items-center gap-x-2 py-2 px-2 rounded-lg duration-150 hover:text-indigo-600 hover:bg-gray-50 active:bg-gray-100 font-medium'
-								onClick={() => setActiveTab(idx)}
-							>
-								{item.icon}
-								{item.name}
-							</button>
-						</li>
+							{item.icon}
+							{item.name}
+						</button>
 					))}
 				</ul>
 			</div>
