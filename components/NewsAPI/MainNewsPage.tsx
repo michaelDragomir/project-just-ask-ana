e061f3fd5f1bc25bbca6e9366ecdd3dee7d3da42
Michael Dragomir <michaeldragomir@Michaels-Laptop.lan>
2023-06-23 17:03:01 -0700

    fixed error issue with search bar

--- components/NewsAPI/MainNewsPage.tsx
+++ components/NewsAPI/MainNewsPage.tsx
@@ -2,20 +2,65 @@
 
 import { useState, useEffect } from 'react';
 import SearchResultsTabs from '@/components/NewsAPI/SearchResultsTabs';
+import { SlMagnifier } from 'react-icons/sl';
 
 const MainNewsPage = () => {
+	const [popularNewsArticles, setPopularNewsArticles] = useState<any>([]);
+	const [inputFieldValue, setInputFieldValue] = useState<string>('');
+
+	const enodedURLValue = encodeURIComponent(
+		inputFieldValue.replace(/\s+/g, '+'),
+	);
+
+	const getNewsByPopularity = async (queries: any) => {
+		console.log(
+			'URL WITH PARAMS---',
+			`https://newsapi.org/v2/everything?q=${queries}&domains=cnn.com,msnbc.com&sortBy=popularity`,
+		);
+		const response = await fetch(
+			`https://newsapi.org/v2/everything?q=${queries}&domains=cnn.com,msnbc.com&sortBy=popularity`,
+			{
+				headers: {
+					'x-api-key': '373a44e44cde4b79bca78c553bcead34',
+				},
+				next: { revalidate: 7200 },
+			},
+		);
+		const { articles } = await response.json();
+
+		setPopularNewsArticles(articles);
+
+		return articles;
+	};
+
+	const search = (e: any) => {
+		e.preventDefault();
+
+		if (!inputFieldValue.length) return;
+
+		console.log('inside search', inputFieldValue);
+
+		getNewsByPopularity(enodedURLValue);
+		setInputFieldValue('');
+	};
+
+	console.log('articles-POPULAR', popularNewsArticles);
+	console.log('inputFieldValue', inputFieldValue);
+
 	return (
 		<>
 			<div>
-				<header className='mx-auto w-3/4 h-auto pt-4 text-2xl text-center text-slate-700 mb-7'>
-					<label className='relative block'>
-						<input
-							className=' mx-auto placeholder:italic placeholder:text-slate-400 block bg-white border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm'
-							placeholder='Search for anything...'
-							type='text'
-						/>
-					</label>
-				</header>
+				<input
+					required
+					className=' mx-auto placeholder:italic placeholder:text-slate-400 block bg-white border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm'
+					placeholder='Search...'
+					type='text'
+					value={inputFieldValue}
+					onChange={(e) => setInputFieldValue(e.target.value)}
+				/>
+				<button className='relative' type='submit' onClick={search}>
+					<SlMagnifier className='w-4 h-4 absolute bottom-7 left-[70px] text-slate-400' />
+				</button>
 				<SearchResultsTabs />
 			</div>
 		</>
--- components/NewsAPI/TabSlider.tsx
+++ components/NewsAPI/TabSlider.tsx
@@ -5,11 +5,6 @@ import { useState } from 'react';
 const TabSlider = ({ tabItems }: { tabItems: any }) => {
 	const [activeTab, setActiveTab] = useState<any>(0);
 
-	// const test = (item: any) => {
-	// 	setActiveTab(item);
-	// 	console.log('handleclick', activeTab);
-	// };
-
 	return (
 		<>
 			<div className='px-4 md:px-8 w-full flex justify-center'>
@@ -20,11 +15,12 @@ const TabSlider = ({ tabItems }: { tabItems: any }) => {
 					{tabItems.map((item: any, idx: any) => (
 						<button
 							role='tab'
+							key={idx}
 							aria-selected={activeTab == idx ? true : false}
 							aria-controls={`tabpanel-${idx + 1}`}
 							className={`flex items-center gap-x-2 py-2 border-b px-2 rounded-lg duration-150 hover:text-indigo-600 hover:bg-gray-50 active:bg-gray-100 font-medium ${
 								activeTab == idx
-									? 'border-indigo-600 text-indigo-600'
+									? 'border-sky-600 text-sky-600'
 									: 'border-white text-gray-500'
 							}`}
 							onClick={() => setActiveTab(idx)}
