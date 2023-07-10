e05979f9950c43c7ec021e250d96d98770ad697e
Michael Dragomir <michaeldragomir@Michaels-Laptop.lan>
2023-07-10 11:41:58 -0700

    updated npm, added moment.js

--- components/NewsAPI/ArticleDetailPage.tsx
+++ components/NewsAPI/ArticleDetailPage.tsx
@@ -1,16 +1,20 @@
+import moment from 'moment';
+
 const ArticleDetailPage = ({ context }: any) => {
 	const {
 		searchParams: { title, author, description, publishedAt, content },
 	} = context;
 
+	const sanitizedPublishedDate = moment(publishedAt).format('MMMM Do YYYY');
+
 	return (
-		<>
+		<div className=''>
 			<div>Title: {title}</div>
 			<div> by: {author}</div>
 			<div>Description: {description}</div>
-			<div>Published: {publishedAt}</div>
+			<div>Published: {sanitizedPublishedDate}</div>
 			<div>Content: {content}</div>
-		</>
+		</div>
 	);
 };
 
